27ecab1dc6a1f5e6273baf197ac2a2b4427d5716
Khummeung Wattanasaroj <62160244@go.buu.ac.th>
2021-12-26 19:08:22 +0700

    create rounter to MyGame and create table in this page

---
+++ src/components/OXTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <table>
+      <tr v-for="(line, lineId) in table" :key="lineId">
+        <td v-for="(item, itemId) in line" :key="itemId">{{ item }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      table: [
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-']
+      ]
+    }
+  }
+}
+</script>
+<style></style>
