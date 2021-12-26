a0d2928c537915a9b5b06442118c3b63e39e91f9
Khummeung Wattanasaroj <62160244@go.buu.ac.th>
2021-12-26 20:40:49 +0700

    create method to change table element to 'O'

---
+++ src/components/OXCell.vue
@@ -0,0 +1,17 @@
+<template>
+  <div @click="clickCell">{{ player }}</div>
+</template>
+<script>
+export default {
+  props: ['player', 'row', 'col'],
+  methods: {
+    clickCell (event) {
+      // console.log(event)
+      // console.log('click' + this.row + ' ' + this.col)
+      if(player !== '-') return
+      this.$emit('cell-click', this.row, this.col)
+    }
+  }
+}
+</script>
+<style></style>
--- src/components/OXTable.vue
+++ src/components/OXTable.vue
@@ -2,20 +2,38 @@
   <div>
     <table>
       <tr v-for="(line, lineId) in table" :key="lineId">
-        <td v-for="(item, itemId) in line" :key="itemId">{{ item }}</td>
+        <td v-for="(item, itemId) in line" :key="itemId">
+          <OXCell
+            :player="item"
+            :row="lineId"
+            :col="itemId"
+            @cell-click="cellClick"
+          />
+        </td>
       </tr>
     </table>
   </div>
 </template>
 <script>
+import OXCell from './OXCell.vue'
 export default {
   data () {
     return {
+      turn: 'O',
       table: [
         ['-', '-', '-'],
         ['-', '-', '-'],
         ['-', '-', '-']
       ]
+    }
+  },
+  components: {
+    OXCell
+  },
+  methods: {
+    cellClick (row, col) {
+      console.log('Cell click ' + row + ' ' + col)
+      this.table[row][col] = this.turn
     }
   }
 }
